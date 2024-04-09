f254ca4a283a77c79f54084ed6e6bb948ea77027
Angel Rodriguez <79729254+arodriguez47-source@users.noreply.github.com>
2024-04-09 14:25:02 -0500

    map initial implementation

---
+++ map.js
@@ -0,0 +1,12 @@
+function initmap() {
+  alert('it works');
+  //map = new google.maps.Map(document.getElementById('map'), {
+    //center: {lat: -34.397, lng: 150.644},
+    //zoom: 8
+  //});
+
+  //<script src="https://maps.googleapis.com/maps/api/js?AIzaSyBM1F-BpX660SBK3-Vw9O9f7AEvTAIH8rI&callback=initMap"
+    //async defer></script>
+}
+
+google.maps.event.addDomListener(window, 'load', init);
